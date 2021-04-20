076f25e1b7a9bb274b62b23563d81330c999ed2b
Robert Hastings <hastings@moesol.com>
2021-04-20 16:23:27 -0700

    watch files and rebuild on save
    switch from http-server to rollup-serve
    makes `npm start` work` after a `npm install`

--- es-extension-examples/ext-example-rollup/rollup.config.js
+++ es-extension-examples/ext-example-rollup/rollup.config.js
@@ -2,6 +2,7 @@ import typescript from 'rollup-plugin-typescript2'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import replace from '@rollup/plugin-replace'
+import serve from 'rollup-plugin-serve'
 
 /*
  * NOTE: this setup bundles react and react-dom with this extension.
@@ -28,6 +29,13 @@ export default {
         nodeResolve(),
         commonjs({
             include: /node_modules/,
+        }),
+        process.env.DO_SERVE && serve({
+            port: 9092,
+            contentBase: 'build',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+            },
         }),
     ],
 }
