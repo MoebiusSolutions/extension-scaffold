9bd43f64645d5860207bdfb9b064587fd3aeea6e
Robert Hastings <hastings@moesol.com>
2021-09-30 20:37:17 -0700

    Initial es-home page.
    Partially converts the es-demo to using data driving configurations.

---
+++ es-home/src/index.ts
@@ -0,0 +1,40 @@
+import { extensionScaffold } from '@gots/es-runtime/build/es-api'
+
+
+async function loadExtensions() {
+  extensionScaffold.boot(document.getElementById('demo-grid-container'))
+
+  await extensionScaffold.loadExtensions([
+    'http://localhost:9091/dist/ext-react-snowpack.js',
+    'http://localhost:9092/ext-react-rollup.js',
+    'http://localhost:5000/build/ext-svelte-rollup.js',
+    'http://localhost:9093/ext-react-webpack.js',
+    'http://localhost:9094/dist/ext-lit-element.js',
+  ])
+}
+
+loadExtensions()
+
+// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
+// Learn more: https://snowpack.dev/concepts/hot-module-replacement
+if (import.meta.hot) {
+  import.meta.hot.accept();
+}
+
+async function loadAppConfig() {
+  if (!location.hash) {
+    location.hash = '#example'
+  }
+  const app = location.hash.replace('#', '').replace('..', '')
+  console.log('app', app)
+
+  const rsp = await fetch(`${app}.json`)
+  const config = await rsp.json()
+
+  if (config.title) {
+    document.title = config.title
+  }
+  console.log(`app config`, config)
+}
+
+loadAppConfig()
