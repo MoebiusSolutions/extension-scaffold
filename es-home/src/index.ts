2860fddf61b4c5a16c6966c54eda38efaaed121f
Robert Hastings <hastings@moesol.com>
2021-09-30 22:05:58 -0700

    Converted es-demo to es-home data driven configuration
    Still need to apply the layout from the application config json

--- es-home/src/index.ts
+++ es-home/src/index.ts
@@ -1,20 +1,5 @@
 import { extensionScaffold } from '@gots/es-runtime/build/es-api'
 
-
-async function loadExtensions() {
-  extensionScaffold.boot(document.getElementById('demo-grid-container'))
-
-  await extensionScaffold.loadExtensions([
-    'http://localhost:9091/dist/ext-react-snowpack.js',
-    'http://localhost:9092/ext-react-rollup.js',
-    'http://localhost:5000/build/ext-svelte-rollup.js',
-    'http://localhost:9093/ext-react-webpack.js',
-    'http://localhost:9094/dist/ext-lit-element.js',
-  ])
-}
-
-loadExtensions()
-
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://snowpack.dev/concepts/hot-module-replacement
 if (import.meta.hot) {
@@ -34,7 +19,13 @@ async function loadAppConfig() {
   if (config.title) {
     document.title = config.title
   }
-  console.log(`app config`, config)
+
+  if (config.extensions) {
+    extensionScaffold.boot(document.getElementById('demo-grid-container'))
+    await extensionScaffold.loadExtensions(config.extensions)
+  } else {
+    console.error('Application configuration missing extensions:', app)
+  }
 }
 
 loadAppConfig()
