ba77ae4dc8db96d427a138c70db22552307c6b4c
Robert Hastings <hastings@moesol.com>
2021-10-21 12:35:13 -0700

    Upgraded all to @gots/es-runtime@1.6.0
    Added skipped code for iframe-example.

---
+++ es-extension-examples/iframe-example/src/index.ts
@@ -0,0 +1,15 @@
+/**
+ * This file is just a silly example to show everything working in the browser.
+ * When you're ready to start on your site, clear the file. Happy hacking!
+ **/
+
+import confetti from 'canvas-confetti';
+
+function handleLoad() {
+  confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
+    resize: true,
+    useWorker: true,
+  })({ particleCount: 200, spread: 200 });
+}
+// window.addEventListener('load', handleLoad)
+window.addEventListener('message', console.log)
