a947cb70858f72ff968335a297544490c433d6b1
Robert Hastings <hastings@moesol.com>
2021-12-01 00:13:06 -0800

    Polish debug console.
    icons/colors, close icon.

--- es-home/src/extensions/console-extension.ts
+++ es-home/src/extensions/console-extension.ts
@@ -18,6 +18,27 @@ const MAX_LOGS = 100
 class EsDebugConsole extends Tonic {
   stylesheet() {
     return `
+    .header {
+      background: var(--es-theme-surface);
+      position: absolute;
+      right: 8px;
+    }
+    .close {
+      background: rgba(255,255,255,0.03);
+      fill: var(--es-theme-text-secondary-on-background);
+      display: flex;
+      justify-content: end;
+      padding: 4px;
+      cursor: pointer;
+    }
+    .close:hover {
+      fill: var(--es-theme-text-primary-on-background);
+      background: rgba(0, 0, 0, 0.3);
+    }
+    .close svg {
+      width: 16px;
+      height: 16px;
+    }
     .container {
       position: absolute;
       top: 0px;
@@ -32,6 +53,33 @@ class EsDebugConsole extends Tonic {
       overflow: auto;
       resize: vertical;
       max-height: -webkit-fill-available;
+    }
+    .console pre {
+      margin: 4px;
+      color: var(--es-theme-text-primary-on-background);
+    }
+    .console pre svg {
+      width: 12px;
+      margin-right: 2px;
+      margin-left: 2px;
+      vertical-align: middle;
+    }
+    .console pre.debug {
+      border: 1px solid rgba(0,0,255,0.2);
+      background: rgba(0,0,255,0.04);
+    }
+    .console pre.info {
+      fill: var(--es-theme-text-secondary-on-background);
+    }
+    .console pre.warn {
+      border: 1px solid rgba(255,255,0,0.2);
+      background: rgba(255,255,0,0.04);
+      fill: yellow;
+    }
+    .console pre.error {
+      border: 1px solid rgba(255,0,0,0.2);
+      background: rgba(255,0,0,0.04);
+      fill: darkred;
     }
 
     ::-webkit-scrollbar {
@@ -51,43 +99,109 @@ class EsDebugConsole extends Tonic {
     }
     `
   }
+  click(e: MouseEvent) {
+    if (e.target instanceof Element) {
+      if (e.target.closest('.close')) {
+        const e = new CustomEvent('console-close')
+        this.dispatchEvent(e)
+      }
+    }
+  }
   render() {
-    const lines = LOGS.records.map(r => r.args.join(', ')).map(l => this.html`<div>${l}</div>`)
+    const icons = {
+      warn: this.html`<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z"/></svg>`,
+      error: this.html`<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"/></svg>`,
+      debug: this.html`<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"></svg>`,
+      info: this.html`<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>`,
+      log: this.html`<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"></svg>`,
+
+    }
+    const lines = LOGS.records.map(r => {
+      const m = r.args.join(', ')
+      const i: keyof typeof icons = r.level as any
+      return this.html`<pre class="${r.level}">${icons[i]}${m}</pre>`
+    })
     return this.html`
-    <div class="container">
-      <div class="console">${lines}</div>
-    </div>
+      <div class="container">
+        <div class="header">
+          <div class="close">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>
+          </div>
+        </div>
+        <div class="console">
+          ${lines}
+        </div>
+      </div>
     `
   }
 }
 
 class EsDebugConsoleRibbonPanel extends Tonic {
-  private open = false
+  private debugConsole: EsDebugConsole | null = null
 
   async doOpen() {
     const div = await extensionScaffold.chrome.panels.addPanel({
       id: HELP_CONSOLE_DISPLAY,
       location: 'portal-wide',
     })
-    const c = document.createElement('es-debug-console')
-    div.appendChild(c)
+    const debugConsole = document.createElement('es-debug-console')
+    debugConsole.addEventListener('console-close', () => {
+      this.doClose()
+    })
+    div.appendChild(debugConsole as Node)
+    
+    this.debugConsole = debugConsole as any
   }
   doClose() {
     extensionScaffold.chrome.panels.removePanel(HELP_CONSOLE_DISPLAY)
+    this.debugConsole = null
   }
 
   click(e: MouseEvent) {
     if (e.target instanceof Element) {
       if (e.target.closest('es-ribbon-button[name="Console"]')) {
-        this.open = !this.open
-        if (this.open) {
+        if (this.debugConsole === null) {
           this.doOpen()
         } else {
           this.doClose()
         }
       } else if (e.target.closest('es-ribbon-button[name="Crash"]')) {
         throw new Error('Not handled')
+      } else if (e.target.closest('es-ribbon-button[name="Bad Fetch"]')) {
+        fetch('http://hastings-foundation.org')
+      } else if (e.target.closest('es-ribbon-button[name="Clear"]')) {
+        LOGS.records = []
+        this.logPush('log', ['Console cleared.'])
       }
+    }
+  }
+  logPush(level: string, args: any[]) {
+    LOGS.records.push({ level, args })
+    while (LOGS.records.length > MAX_LOGS) {
+      LOGS.records.shift()
+    }
+    this.debugConsole?.reRender()
+  }
+  connected() {
+    try {
+      const levels = ['debug', 'info', 'log', 'warn', 'error']
+      const self = this
+      levels.forEach(level => {
+        const origFn: any = console[level as keyof Console]
+        function newFn(...args: any) { 
+          origFn.apply(console, args)
+          self.logPush(level, args)
+        }
+        console[level as keyof Console] = newFn as any
+      })
+      window.addEventListener('error', (evt: ErrorEvent) => {
+        this.logPush('error', [evt.message, evt.filename, evt.lineno, evt.error])
+      })
+      window.addEventListener('unhandledrejection', (evt: PromiseRejectionEvent) => {
+        this.logPush('error', ['Unhandled promise rejection', evt.reason.stack || evt.reason])
+      })
+    } catch (e) {
+      console.error('Failed to hook console functions')
     }
   }
   render() {
@@ -96,19 +210,14 @@ class EsDebugConsoleRibbonPanel extends Tonic {
         <es-ribbon-button name="Console">
           <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M7 5h10v2h2V3c0-1.1-.9-1.99-2-1.99L7 1c-1.1 0-2 .9-2 2v4h2V5zm8.41 11.59L20 12l-4.59-4.59L14 8.83 17.17 12 14 15.17l1.41 1.42zM10 15.17L6.83 12 10 8.83 8.59 7.41 4 12l4.59 4.59L10 15.17zM17 19H7v-2H5v4c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2v-4h-2v2z"/></svg>
         </es-ribbon-button>
-        <es-ribbon-button name="Crash">
-        </es-ribbon-button>
+        <es-ribbon-button name="Crash"></es-ribbon-button>
+        <es-ribbon-button name="Bad Fetch"></es-ribbon-button>
+        <es-ribbon-button name="Clear"></es-ribbon-button>
       </es-ribbon-section>
     `
   }
 }
 
-function logPush(level: string, args: any[]) {
-  LOGS.records.push({ level, args })
-  while (LOGS.records.length > MAX_LOGS) {
-    LOGS.records.shift()
-  }
-}
 //
 // Collects console output and unhandled errors into a ring buffer
 //
@@ -122,21 +231,4 @@ export async function activate(scaffold: ExtensionScaffoldApi) {
   const s = document.createElement('es-debug-console-ribbon-panel')
   div.innerText = ''
   div.appendChild(s)
-
-  try {
-    const levels = ['debug', 'info', 'log', 'warn', 'error']
-    levels.forEach(level => {
-      const origFn: any = console[level as keyof Console]
-      function newFn(...args: any) { 
-        origFn.apply(console, args)
-        logPush(level, args)
-      }
-      console[level as keyof Console] = newFn as any
-    })
-    window.addEventListener('error', (evt: ErrorEvent) => {
-      logPush('error', [evt.message, evt.filename, evt.lineno, evt.error])
-    })
-  } catch (e) {
-    console.error('Failed to hook console functions')
-  }
 }
