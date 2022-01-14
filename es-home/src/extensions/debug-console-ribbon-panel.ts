8e219d192a4bf93f0dda653b2c37b416f6579f96
Robert Hastings <hastings@moesol.com>
2022-01-13 22:27:20 -0800

    Host broadcast bus from `es-home`.
    Simplified installation and configuration by removing
    the need for another page to host the small page/js.

--- es-home/src/extensions/debug-console-ribbon-panel.ts
+++ es-home/src/extensions/debug-console-ribbon-panel.ts
@@ -1,6 +1,7 @@
 import Tonic from "@optoolco/tonic"
 import { EsDebugConsole, LOGS, MAX_LOGS } from "./debug-console"
 import { extensionScaffold } from '@gots/es-runtime/build/es-api'
+import { publishJson } from '@gots/noowf-inter-widget-communication';
 
 const HELP_CONSOLE_DISPLAY = 'help.console.display'
 
@@ -25,23 +26,33 @@ export class EsDebugConsoleRibbonPanel extends Tonic {
     extensionScaffold.chrome.panels.removePanel(HELP_CONSOLE_DISPLAY)
     this.debugConsole = null
   }
+  private doPublish(target: Element) {
+    const section = target.closest('es-ribbon-section')
+    const message: HTMLInputElement = section?.querySelector('[name="message"]') as any
+    publishJson('es.ping.topic', {
+      message: message?.value
+    })
+  }
 
   pointerdown(e: PointerEvent) {
     this.wasOpen = this.debugConsole !== null
   }
   click(e: MouseEvent) {
     if (e.target instanceof Element) {
+      const target = e.target
       if (e.target.closest('es-ribbon-button[label="Console"]')) {
         if (!this.wasOpen) {
           this.doOpen()
         }
-      } else if (e.target.closest('es-ribbon-button[data-name="Crash"]')) {
+      } else if (e.target.closest('[data-name="Crash"]')) {
         throw new Error('Not handled')
-      } else if (e.target.closest('es-ribbon-button[data-name="Bad Fetch"]')) {
+      } else if (e.target.closest('[data-name="Bad Fetch"]')) {
         fetch('http://hastings-foundation.org')
-      } else if (e.target.closest('es-ribbon-button[data-name="Clear"]')) {
+      } else if (e.target.closest('[data-name="Clear"]')) {
         LOGS.records = []
         this.logPush('log', ['Console cleared.'])
+      } else if (e.target.closest('[data-name="Publish"]')) {
+        this.doPublish(target)
       }
     }
   }
@@ -95,11 +106,16 @@ export class EsDebugConsoleRibbonPanel extends Tonic {
         <es-ribbon-button label="Console">
           <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M7 5h10v2h2V3c0-1.1-.9-1.99-2-1.99L7 1c-1.1 0-2 .9-2 2v4h2V5zm8.41 11.59L20 12l-4.59-4.59L14 8.83 17.17 12 14 15.17l1.41 1.42zM10 15.17L6.83 12 10 8.83 8.59 7.41 4 12l4.59 4.59L10 15.17zM17 19H7v-2H5v4c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2v-4h-2v2z"/></svg>
         </es-ribbon-button>
-        <div style="display: flex; flex-direction: column;">
+        <es-ribbon-column>
           <es-ribbon-button styles="left" data-name="Crash">    <div>Crash</div>    </es-ribbon-button>
           <es-ribbon-button styles="left" data-name="Bad Fetch"><div>Bad Fetch</div></es-ribbon-button>
           <es-ribbon-button styles="left" data-name="Clear">    <div>Clear</div>    </es-ribbon-button>
-      </div>
+        </es-ribbon-column>
+        <es-ribbon-column>
+          <div>es.ping.topic</div>
+          <input name="message" placeholder="message">
+          <es-ribbon-button-small data-name="Publish" label="Publish"></es-ribbon-button>
+        </es-ribbon-column>
       </es-ribbon-section>
     `
   }
