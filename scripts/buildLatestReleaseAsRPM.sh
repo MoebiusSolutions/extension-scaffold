8de0f543626afd0e70e201b79518004fe8a9543e
Linda Rae <linda.rae.ctr@navy.mil>
2022-01-13 18:00:45 -0800

    updates

--- scripts/buildLatestReleaseAsRPM.sh
+++ scripts/buildLatestReleaseAsRPM.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 git fetch --tags
+SCRIPT_DIR=$(dirname `readlink -f "$0"`)
 LATEST_RELEASE=$(git describe --tags `git rev-list --tags --max-count=1`)
 echo "Latest Release: ${LATEST_RELEASE}"
-git switch ${LATEST_RELEASE} 
-cd ../es-home
+git checkout ${LATEST_RELEASE} 
+cd ${SCRIPT_DIR}/../es-home
 echo "Building release .."
 npm ci
 npm run build
-cd ../buildrpm
+cd ${SCRIPT_DIR}/../buildrpm
 echo "Building RPM .."
 ./build.sh   
