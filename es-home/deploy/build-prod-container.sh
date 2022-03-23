e38f32bea3f29cbaa7f668892bc469156c5fe081
Robert Hastings <hastings@moesol.com>
2022-03-22 21:00:37 -0700

    fix rel path

--- es-home/deploy/build-prod-container.sh
+++ es-home/deploy/build-prod-container.sh
@@ -13,7 +13,7 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 NPM_CONFIG_USERCONFIG=${NPM_CONFIG_USERCONFIG:=${HOME}/.npmrc}
 
 if [[ -z "$NOSUDO" ]]; then
-    SUDO="sudo"
+    SUDO="sudo DOCKER_BUILDKIT=1"
 else
     SUDO=''
 fi
@@ -24,6 +24,6 @@ fi
 set -x
 export DOCKER_BUILDKIT=1
 ${SUDO} docker build -t extension-scaffold/es-home \
-  "$SCRIPT_DIR/../../../extension-scaffold/es-home/" -f "$SCRIPT_DIR/Dockerfile" \
+  "$SCRIPT_DIR/.." -f "$SCRIPT_DIR/Dockerfile" \
   --secret id=npmrc,src=${NPM_CONFIG_USERCONFIG} "$@"
 set +x
