1e4f4eba07356f66c7ab96b5d3818dba2fc078e9
Linda Rae <linda.rae.ctr@navy.mil>
2022-06-07 10:51:20 -0700

    updates to include kb and build using rush

--- scripts/buildBranchRPM.sh
+++ scripts/buildBranchRPM.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 git fetch --tags
 SCRIPT_DIR=$(dirname `readlink -f "$0"`)
-LATEST_RELEASE=$( grep version ../es-runtime/package.json |tr -d version:,\"\ )
-echo "Latest Release Version: ${LATEST_RELEASE}"
-cd ${SCRIPT_DIR}/../es-home
-npm ci
-npm run build
-cd ${SCRIPT_DIR}/../es-common-extensions
-npm ci
-npm run build
+BRANCH=$( grep version ../es-runtime/package.json |tr -d version:,\"\ )
+echo "current branch: ${BRANCH}"
+cd ${SCRIPT_DIR}/..
+git pull
+git clean -f -x -xd
+rush update
+rush build
 cd ${SCRIPT_DIR}/../rpmbuild
 echo "Building RPM .."
 ./build.sh   
