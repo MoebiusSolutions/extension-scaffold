410929ce8130c15a12eff3251b94f1819447731f
Linda Rae <linda.rae.ctr@navy.mil>
2022-06-07 10:53:29 -0700

    fix typo

--- scripts/buildLatestReleaseAsRPM.sh
+++ scripts/buildLatestReleaseAsRPM.sh
@@ -3,7 +3,7 @@ git fetch --tags
 SCRIPT_DIR=$(dirname `readlink -f "$0"`)
 LATEST_RELEASE=$( grep version ../es-runtime/package.json |tr -d version:,\"\ )
 echo "Latest Release Version: ${LATEST_RELEASE}"
-cd ${SCRIPT_DIR}/
+cd ${SCRIPT_DIR}/..
 git checkout release
 git pull
 git clean -f -x -d
